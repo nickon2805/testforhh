35656222d7f5c21ca418484049ab6fff6773d1bc
Nikita <80483732+nickon2805@users.noreply.github.com>
2023-01-23 19:18:15 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,145 @@
+// burger-menu
+const openMenu = document.querySelector('.header-top__burger-btn');
+const closeMenu = document.querySelector('.header-top__burger-btnClose');
+const contentMenu = document.querySelector('.menu');
+
+openMenu.addEventListener('click', openHeaderMenu);
+
+function openHeaderMenu() {
+  contentMenu.style.display = 'block';
+  openMenu.style.display = 'none';
+  closeMenu.style.display = 'block';
+  attachMenuEvent();
+}
+
+function attachMenuEvent() {
+  closeMenu.addEventListener('click', closeHeaderMenu);
+}
+
+function closeHeaderMenu() {
+  contentMenu.style.display = 'none';
+  closeMenu.style.display = 'none';
+  openMenu.style.display = 'block';
+  detachMenuEvents();
+}
+
+function detachMenuEvents() {
+  closeMenu.removeEventListener('click', closeHeaderMenu)
+}
+
+// slider
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("services-slider__item");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "flex";
+}
+
+// popup
+const modals = () => {
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay = true) {
+    const trigger = document.querySelectorAll(triggerSelector),
+      modal = document.querySelector(modalSelector),
+      close = document.querySelector(closeSelector),
+      windows = document.querySelectorAll('[data-modal]'),
+      scroll = calcScroll();
+
+    trigger.forEach(item => {
+      item.addEventListener('click', (e) => {
+        if (e.target) {
+          e.preventDefault();
+        }
+
+        windows.forEach(item => {
+          item.style.display = 'none';
+        });
+
+        modal.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
+      });
+    });
+
+    close.addEventListener('click', () => {
+      windows.forEach(item => {
+        item.style.display = 'none';
+      });
+
+      modal.style.display = 'none';
+      document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
+    });
+
+    modal.addEventListener('click', (e) => {
+      if (e.target === modal && closeClickOverlay) {
+        windows.forEach(item => {
+          item.style.display = 'none';
+        });
+
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
+      }
+    })
+  }
+
+  function calcScroll() {
+    let div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
+  }
+
+  bindModal('.header-top__btn', '.modal-reception', '.modal-reception .modal-reception__close-btn');
+  bindModal('.menu__btn-mob', '.modal-reception', '.modal-reception .modal-reception__close-btn');
+};
+
+modals();
+// end popup
+
+// mail-php-js
+
+$(document).ready(function () {
+
+  //E-mail Ajax Send
+  $("form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+
+});
