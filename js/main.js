89736ba293f01cbf69298aa6e2a52530752d3c82
Nikita <80483732+nickon2805@users.noreply.github.com>
2023-01-23 21:36:17 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -121,25 +121,5 @@ const modals = () => {
 modals();
 // end popup
 
-$(document).ready(function () {
-
-  //E-mail Ajax Send
-  $("form").submit(function () { //Change
-    var th = $(this);
-    $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
-      data: th.serialize()
-    }).done(function () {
-      alert("Thank you!");
-      setTimeout(function () {
-        // Done Functions
-        th.trigger("reset");
-      }, 1000);
-    });
-    return false;
-  });
-
-});
 
 
